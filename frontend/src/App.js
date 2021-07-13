782d535b9a6a63fbae920aacdb9d933f3ad018eb
Mitchell Debrah <debrahmitch@Mitchells-MBP.fios-router.home>
2021-07-13 02:12:56 -0400

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,5 @@
 
 import React from "react"
-import Homepage from "./chapters/Homepage";
-import Navbar from "./components/Navbar";
-import Singlepage from "./chapters/Singlepage";
-import Author from "./chapters/Author.jsx";
-import SettingsAcct from "./chapters/SettingsAcct";
-import Loginpage from "./chapters/Loginpage"
-import Registerpage from "./chapters/Registerpage";
 
 
 
