b41a0e41151b7d3887516ebd8de0961578cc0111
Mitchell Debrah <debrahmitch@Mitchells-MBP.fios-router.home>
2021-07-13 01:56:15 -0400

    users

--- backend/controllers/bloggers.js
+++ backend/controllers/bloggers.js
@@ -2,3 +2,27 @@ const router = require("express").Router();
 const Blogger = require("../models/Blogger");
 const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
+
+//update
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    if (req.body.password) {
+      const hashSalt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, hashSalt);
+    }
+    try {
+      const userUpdate = await Blogger.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(userUpdate);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(401).json({msg: "update your account only!"});
+  }
+});
