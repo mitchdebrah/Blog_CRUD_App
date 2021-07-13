9c6f5df724ab6ab0b621f7cb649be74b8e79abe9
Mitchell Debrah <debrahmitch@Mitchells-MBP.fios-router.home>
2021-07-13 02:11:36 -0400

    authors page created

--- frontend/src/chapters/Author.jsx
+++ frontend/src/chapters/Author.jsx
@@ -1,7 +1,20 @@
-
-
 import { useContext, useState } from "react";
 import axios from "axios";
 import { Context } from "../medium/Context";
 import "../index.css";
 
+const Author = ()=> {
+  const [title, setTitle] = useState("");
+  const [description, setDesc] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      username: user.username,
+      title,
+      description,
+    };
+
+    
