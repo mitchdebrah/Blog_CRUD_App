f589399257b71c8389548284e33ee555f34257a9
Mitchell Debrah <debrahmitch@Mitchells-MBP.fios-router.home>
2021-07-13 02:05:58 -0400

    authentication and routing created

--- backend/controllers/bloggers.js
+++ backend/controllers/bloggers.js
@@ -26,3 +26,36 @@ router.put("/:id", async (req, res) => {
     res.status(401).json({msg: "update your account only!"});
   }
 });
+
+//delet
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await Blogger.findById(req.params.id);
+      try {
+        await Post.deleteMany({ username: user.username });
+        await Blogger.findByIdAndDelete(req.params.id);
+        res.status(200).json({msg:"Blogger deleted..."});
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json({msg:"Blogger not found!"});
+    }
+  } else {
+    res.status(401).json({msg:" delete only your account!"});
+  }
+});
+
+//get user or page user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await Blogger.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
