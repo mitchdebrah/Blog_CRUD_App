9217398aaffa4bec8f40f222f4b5a33decf4cd67
Mitchell Debrah <debrahmitch@Mitchells-MBP.fios-router.home>
2021-07-13 02:04:45 -0400

    authentication created

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -19,3 +19,21 @@ router.post("/register", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await Blogger.findOne({ username: req.body.username });
+    !user && res.status(400).json({msg:"please enter right credentials"});
+
+    const val = await bcrypt.compare(req.body.password, user.password);
+    !val && res.status(400).json({msg:"please enter right credentials"});
+
+    const { password, ...someothers } = user._doc;
+    res.status(200).json(someothers);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
