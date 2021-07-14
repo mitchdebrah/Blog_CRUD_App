32304624502207bd5a3b80c31ce87816e412407e
Mitchell Debrah <debrahmitch@Mitchells-MacBook-Pro.local>
2021-07-14 09:10:36 -0400

    added some bio

--- frontend/src/chapters/Homepage.jsx
+++ frontend/src/chapters/Homepage.jsx
@@ -5,9 +5,9 @@ import Sidebar from "../components/Sidebar";
 import axios from "axios";
 import { useLocation } from "react-router";
 import "../index.css";
-import Oceanvideo from "../components/video/oceanvideo.mp4"
+import Oceanvideo from "../components/video/oceanvideo.mp4";
 
-const Homepage=()=> {
+const Homepage = () => {
   const [posts, setPosts] = useState([]);
   const { search } = useLocation();
   useEffect(() => {
@@ -19,22 +19,25 @@ const Homepage=()=> {
   }, [search]);
   return (
     <div>
-      <video autoPlay
+      <div className="video-container">
+      <video
+        autoPlay
         loop
-        muted 
+        muted
         style={{
           position: "absolute",
           width: "100%",
           left: "50%",
-          top: "20%",
-          height: "50%",
-          objectFit:"cover",
-          transform:"translate(-50%, -50%)",
-          zIndex: "-1"
+          top: "22%",
+          height: "20%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: "-1",
         }}
-           >
-        <source src={Oceanvideo} type="video/mp4"/>
+      >
+        <source src={Oceanvideo} type="video/mp4" />
       </video>
+      </div>
       <Header />
       <div className="homepage">
         <Posts posts={posts} />
@@ -42,5 +45,5 @@ const Homepage=()=> {
       </div>
     </div>
   );
-}
+};
 export default Homepage;
